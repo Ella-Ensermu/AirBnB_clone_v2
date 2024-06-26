db72c02560e1590bab97e78eb1e761f12a3ea16e
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-26 20:59:43 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
+""" Amenity class."""
+
 from sqlalchemy import Column
 from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.place import place_amenity
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
-
-    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
-
+class Amenity (BaseModel, Base):
+   """This is the class for Amenity
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+        name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
