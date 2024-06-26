307b4938a99dba3c9f66b7f358461059daff84ca
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-26 20:58:59 +0300

    Add files via upload

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" Amenity class."""
-
+"""Defines the Amenity class."""
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
-from models.base_model import BaseModel, Base
-from models.base_model import BaseModel
-from models.place import place_amenity
 from sqlalchemy.orm import relationship
 
 
-class Amenity (BaseModel, Base):
-   """This is the class for Amenity
+class Amenity(BaseModel, Base):
+    """Represents an Amenity for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table amenities.
+
     Attributes:
-        name: input name
+        __tablename__ (str): The name of the MySQL table to store Amenities.
+        name (sqlalchemy String): The amenity name.
+        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
